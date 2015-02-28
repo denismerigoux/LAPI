aad198f351e52710126ac6582ef8168443d3f41a
Denis Merigoux <denis.merigoux@gmail.com>
2015-02-28 20:40:20 +0100

    Added fisrt view

--- LAPI/urls.py
+++ LAPI/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from census.views import home
 
-urlpatterns = patterns('',
+urlpatterns = patterns('census.views',
     # Examples:
     # url(r'^$', 'LAPI.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^home/$', 'home'),
+    url(r'^$', 'home'),
 )
