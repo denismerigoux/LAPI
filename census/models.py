cae8a33a703c021cdee2edb97fe16520f2a53e12
Denis Merigoux <denis.merigoux@gmail.com>
2015-03-12 17:14:57 +0100

    new fields in models for support for "conférences" and "amphis 0"

--- census/models.py
+++ census/models.py
@@ -20,7 +20,7 @@ class Amphi(models.Model):
          verbose_name = "Amphithéâtre"
 
 class Course(models.Model):
-	name=models.CharField("Nom",max_length=6)
+	name=models.CharField("Nom",max_length=60)
 	promotion=models.ForeignKey(Promotion,verbose_name="Promotion")
 	enrolled=models.IntegerField("Nombre d'inscrits")
 
@@ -41,20 +41,33 @@ class Professor(models.Model):
 class Lesson(models.Model):
 	course=models.ForeignKey(Course)
 	date=models.DateField("Date")
-	number=models.IntegerField("Numéro de la séance")
-	professor=models.ForeignKey(Professor,verbose_name="Professeur")
+	number=models.IntegerField("Numéro de la séance",blank=True,null=True)
+	professor=models.ForeignKey(Professor,verbose_name="Professeur",blank=True,null=True)
 	amphi=models.ForeignKey(Amphi,verbose_name="Amphithéâtre")
+	title=models.CharField("Titre",max_length=140,blank=True,null=True)
 
 	def __str__(self):
-		return "%s %s (%s,%s)" % (self.course.name,self.number,self.course.promotion,self.date)
+		if ((self.number is not None) & (self.title is not None)):
+			return "%s %s : %s (%s,%s)" % (self.course.name,self.number,self.title,self.course.promotion,self.date)
+		elif (self.number is not None):
+			return "%s %s (%s,%s)" % (self.course.name,self.number,self.course.promotion,self.date)
+		elif (self.title is not None):
+			return "%s : %s (%s,%s)" % (self.course.name,self.title,self.course.promotion,self.date)
+		else:
+			return "%s (%s,%s)" % (self.course.name,self.course.promotion,self.date)
+
 	class Meta:
          verbose_name = "Séance"
 
 class Count(models.Model):
 	lesson=models.ForeignKey(Lesson,verbose_name="Séance")
 	census=models.IntegerField("Nombre d'éléves présents")
+	date=models.DateTimeField("Soumis le",auto_now="True")
+	comment=models.TextField("Commentaires")
+	signature=models.CharField("signature",max_length=120)
+
 
 	def __str__(self):
-		return "%s/%s : %s" % (self.census,self.lesson.course.enrolled,self.lesson)
+		return "%s : %s/%s" % (self.lesson,self.census,self.lesson.course.enrolled)
 	class Meta:
          verbose_name = "Comptage"
